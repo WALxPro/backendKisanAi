42e7316872335d60817c37a2f4c704ede98b0e1f
WALxPro <waleedsheikh15486@gmail.com>
2026-06-21 10:28:53 -0700

    change

--- firebase_admin_setup.py
+++ firebase_admin_setup.py
@@ -1,7 +1,15 @@
+import os
+import json
 import firebase_admin
 from firebase_admin import credentials
-import os
 
-cred = credentials.Certificate(os.path.join(os.path.dirname(__file__), "serviceAccountKey.json"))
+firebase_config = os.getenv("FIREBASE_CONFIG")
+
+if not firebase_config:
+    raise Exception("FIREBASE_CONFIG not found in environment variables")
+
+cred_json = json.loads(firebase_config)
+
+cred = credentials.Certificate(cred_json)
 
 firebase_admin.initialize_app(cred)
